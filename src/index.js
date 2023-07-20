7b8397f1633213cc611848e4dbba082209949c4a
David Barón <103337874+Ldbelop@users.noreply.github.com>
2023-07-19 20:38:59 -0500

    Se termina la sección 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-const navBtn = document.getElementById("nav-btn");
-
-let hamclicked = false;
-
-navBtn.addEventListener('click', () => {
-    hamclicked = !hamclicked;
-    if(hamclicked == true){
-        navBtn.classList.add("hamAnim")
-    } else{
-        navBtn.classList.remove("hamAnim")
-    }
-})
+// const canvas = document.querySelector("canvas")
+// 
+// const ctx = canvas.getContext("2d")
+// 
+// const conicGradient = ctx.createConicGradient(2,150,150);
+// conicGradient.addColorStop(0, )
+// conicGradient.addColorStop(1, )
+// conicGradient.addColorStop(2, )
+// conicGradient.addColorStop(3, )
+// conicGradient.addColorStop(4, )
